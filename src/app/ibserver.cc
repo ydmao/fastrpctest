a3ba37435bb57574cb69f90d19393a5b3f5c4503
Yandong Mao <ydmao@mit.edu>
2014-01-27 11:27:25 -0500

    serving multiple clients for latency tests

--- src/app/ibserver.cc
+++ src/app/ibserver.cc
@@ -1,33 +1,33 @@
 #include "ib.hh"
 #include <string>
 
-static const size_t size = 20;
-static char b[size];
-
-static void read(infb_conn* c) {
-    static int iters = 0;
-    c->read(b, sizeof(b));
-    char eb[size];
-    sprintf(eb, "c_%d", iters);
-    assert(strcmp(eb, b) == 0);
-    ++iters;
-}
-
-static void write(infb_conn* c) {
-    static int iters = 0;
-    sprintf(b, "s_%d", iters++);
-    c->write(b, sizeof(b));
-}
-
-static void process_infb_event(infb_ev_watcher* w, int flags) {
-    if (flags & INFB_EV_READ) {
-	read(w->conn());
-	w->set(INFB_EV_WRITE);
-    } else if (flags & INFB_EV_WRITE) {
-	write(w->conn());
-	w->set(INFB_EV_READ);
+struct client {
+    static constexpr size_t size = 20;
+    char b[size];
+    int nr_;
+    int nw_;
+    client() : nr_(0), nw_(0) {
     }
-}
+    void read(infb_conn* c) {
+        c->read(b, sizeof(b));
+        char eb[size];
+        sprintf(eb, "c_%d", nr_++);
+        assert(strcmp(eb, b) == 0);
+    }
+    void write(infb_conn* c) {
+        sprintf(b, "s_%d", nw_++);
+        c->write(b, sizeof(b));
+    }
+    void event_handler(infb_ev_watcher* w, int flags) {
+        if (flags & INFB_EV_READ) {
+	    read(w->conn());
+	    w->set(INFB_EV_WRITE);
+        } else if (flags & INFB_EV_WRITE) {
+   	    write(w->conn());
+	    w->set(INFB_EV_READ);
+        }
+    }
+};
 
 int main(int argc, char* argv[]) {
     std::string type("async");
@@ -44,17 +44,27 @@ int main(int argc, char* argv[]) {
 	else
 	    f = infb_interrupt_factory::default_instance();
 	infb_conn* c = s.accept(f);
+	client* clt = new client();
 	while (true) {
-	    read(c);
-	    write(c);
+	    clt->read(c);
+	    clt->write(c);
 	}
     } else if (type == "async") {
-        infb_loop* loop = infb_loop::make(infb_provider::default_instance());
-        infb_ev_watcher* w = loop->ev_watcher(s.accept(loop));
-        w->set(process_infb_event);
-        w->set(INFB_EV_READ);
-        while (true)
-	    loop->loop_once();
+	while (true) {
+            infb_loop* loop = infb_loop::make(infb_provider::default_instance());
+	    infb_conn* c = s.accept(loop);
+	    std::thread t([&]{
+                infb_ev_watcher* w = loop->ev_watcher(c);
+	        client* clt = new client;
+	        using std::placeholders::_1;
+	        using std::placeholders::_2;
+                w->set(std::bind(&client::event_handler, clt, _1, _2));
+                w->set(INFB_EV_READ);
+                while (true)
+	            loop->loop_once();
+	    });
+	    t.detach();
+	}
     } else {
 	fprintf(stderr, "Unknown connection type %s\n", type.c_str());
 	fprintf(stderr, "Usage: %s poll|int|async\n", argv[0]);
