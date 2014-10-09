6644ef2e02c19ea74d18e2be0397bf4030aadbfd
Yandong Mao <ydmao@databricks.com>
2014-10-09 13:08:18 -0700

    update bootstrap to install boost-program-options

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git submodule update --init
-sudo apt-get install g++ make libev-dev libprotobuf-dev libprotoc-dev protobuf-compiler libssl-dev libboost-dev libibverbs-dev --yes
+sudo apt-get install g++ make libev-dev libprotobuf-dev libprotoc-dev protobuf-compiler libssl-dev libboost-dev libibverbs-dev libboost-program-options-dev --yes
 if ! test -f fastrpc/config.h ; then
     cd fastrpc && git checkout master && ./configure && cd ..
 fi
